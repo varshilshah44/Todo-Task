007d2fb14739c81ce689ef6c84bca812bc4a5b34
= <=>
2021-01-25 17:13:05 +0530

    updated

--- VarshilShah/TODO_List/Scripts/Todo.js
+++ VarshilShah/TODO_List/Scripts/Todo.js
@@ -28,6 +28,7 @@ async function AddSubtask(taskId, subtaskcount) {
     let taskObj = {
       subtaskId: 0,
       subtaskName: "",
+      subtaskStatus:"TODO"
     };
     taskObj.subtaskId = taskId;
     taskObj.subtaskName = event.target.value;
@@ -37,17 +38,28 @@ async function AddSubtask(taskId, subtaskcount) {
   console.log(taskArr);
 }
 
+function AddingItemintoDom(taskName){
+  const todoData = document.importNode(template.content,"content");
+  const itemLi = todoData.querySelector('li');
+  itemLi.style.backgroundColor = "red";
+  itemLi.querySelector('h3').textContent = taskName;
+  todoOl.appendChild(itemLi);
+}
+
 async function App() {
   let event = await inputEventHandler("Main", taskInput);
   let taskObj = {
     taskId: 0,
     taskName: "",
     taskPriority: "High",
+    taskStatus:"TODO"
   };
   taskObj.taskId = taskObj.taskId + 1;
   taskObj.taskName = event.target.value;
   taskArr.push(taskObj);
   localStorage.setItem('task',JSON.stringify(taskArr));
+  AddingItemintoDom(taskObj.taskName);
+  
   const subtaskPrompt = prompt("How many Subtask You want to add?", "0");
   if (parseInt(subtaskPrompt) >= 0 || parseInt(subtaskPrompt) <= 10) {
     AddSubtask(taskObj.taskId, parseInt(subtaskPrompt));
