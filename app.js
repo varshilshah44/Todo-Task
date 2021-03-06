6c41b092e4a59a329035d75047745a9ac5d8b143
= <=>
2021-03-06 12:22:17 +0530

    updated

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const indexRouter = require('./routes/index');
 app.use(express.static('public'))
 app.use('/',indexRouter);
 
-const port = process.env.PORT || 3000;
 console.log(window.location)
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log("running")
 });
